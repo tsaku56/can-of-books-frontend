31d39beb8b6f034ac2bdba51f9c6d117cf7a25e2
Ross <blindross@gmail.com>
2023-05-17 15:48:38 +0100

    oops

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,9 @@
 export default function About() {
   return (
     <div>
-      <h2>About Cats</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga recusandae quam quos ratione temporibus eaque, error, a incidunt expedita quasi, nihil cupiditate similique non dolor veniam quo
-        quod assumenda pariatur.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga recusandae quam quos ratione temporibus eaque, error, a incidunt expedita quasi, nihil cupiditate similique non dolor veniam quo
-        quod assumenda pariatur.
-      </p>
+      <h2>About Books</h2>
+      <p>Lorem ipsum dolor sit amet consectetur.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga recusandae quam quos ratione temporibus eaque, error, a incidunt expedita quasi, nihil cupiditate.</p>
     </div>
   );
 }
